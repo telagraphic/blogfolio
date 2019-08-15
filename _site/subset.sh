3a564edd8d6be9bb2d2942664cbdaf0b6d2c1690
telagraphic <telagraphic@gmail.com>
2019-08-14 20:35:14 -0400

    added font subsets

--- _site/subset.sh
+++ _site/subset.sh
@@ -2,7 +2,7 @@ pyftsubset "PublicSans-Black.ttf" --output-file="PublicSans-Black.woff2" --flavo
 
 
 
-# With required font features
+# Woff2 - With required font features
 pyftsubset "PublicSans-Black.ttf" --output-file="PublicSans-Black.woff2" --flavor=woff2 --layout-features="ccmp,locl,mark,mkmk,kern" --desubroutinize --unicodes=U+0-10FFFF
 pyftsubset "PublicSans-BlackItalic.ttf" --output-file="PublicSans-BlackItalic.woff2" --flavor=woff2 --layout-features="ccmp,locl,mark,mkmk,kern" --desubroutinize --unicodes=U+0-10FFFF
 pyftsubset "PublicSans-Bold.ttf" --output-file="PublicSans-Bold.woff2" --flavor=woff2 --layout-features="ccmp,locl,mark,mkmk,kern" --desubroutinize --unicodes=U+0-10FFFF
@@ -21,3 +21,24 @@ pyftsubset "PublicSans-SemiBold.ttf" --output-file="PublicSans-SemiBold.woff2" -
 pyftsubset "PublicSans-SemiBoldItalic.ttf" --output-file="PublicSans-SemiBoldItalic.woff2" --flavor=woff2 --layout-features="ccmp,locl,mark,mkmk,kern" --desubroutinize --unicodes=U+0-10FFFF
 pyftsubset "PublicSans-Thin.ttf" --output-file="PublicSans-Thin.woff2" --flavor=woff2 --layout-features="ccmp,locl,mark,mkmk,kern" --desubroutinize --unicodes=U+0-10FFFF
 pyftsubset "PublicSans-ThinItalic.ttf" --output-file="PublicSans-ThinItalic.woff2" --flavor=woff2 --layout-features="ccmp,locl,mark,mkmk,kern" --desubroutinize --unicodes=U+0-10FFFF
+
+
+# Woff - With required font features
+pyftsubset "PublicSans-Black.ttf" --output-file="PublicSans-Black.woff" --flavor=woff --layout-features="ccmp,locl,mark,mkmk,kern" --desubroutinize --unicodes=U+0-10FFFF
+pyftsubset "PublicSans-BlackItalic.ttf" --output-file="PublicSans-BlackItalic.woff" --flavor=woff --layout-features="ccmp,locl,mark,mkmk,kern" --desubroutinize --unicodes=U+0-10FFFF
+pyftsubset "PublicSans-Bold.ttf" --output-file="PublicSans-Bold.woff" --flavor=woff --layout-features="ccmp,locl,mark,mkmk,kern" --desubroutinize --unicodes=U+0-10FFFF
+pyftsubset "PublicSans-BoldItalic.ttf" --output-file="PublicSans-BoldItalic.woff" --flavor=woff --layout-features="ccmp,locl,mark,mkmk,kern" --desubroutinize --unicodes=U+0-10FFFF
+pyftsubset "PublicSans-ExtraBold.ttf" --output-file="PublicSans-ExtraBold.woff" --flavor=woff --layout-features="ccmp,locl,mark,mkmk,kern" --desubroutinize --unicodes=U+0-10FFFF
+pyftsubset "PublicSans-ExtraBoldItalic.ttf" --output-file="PublicSans-ExtraBoldItalic.woff" --flavor=woff --layout-features="ccmp,locl,mark,mkmk,kern" --desubroutinize --unicodes=U+0-10FFFF
+pyftsubset "PublicSans-ExtraLight.ttf" --output-file="PublicSans-ExtraLight.woff" --flavor=woff --layout-features="ccmp,locl,mark,mkmk,kern" --desubroutinize --unicodes=U+0-10FFFF
+pyftsubset "PublicSans-ExtraLightItalic.ttf" --output-file="PublicSans-ExtraLightItalic.woff" --flavor=woff --layout-features="ccmp,locl,mark,mkmk,kern" --desubroutinize --unicodes=U+0-10FFFF
+pyftsubset "PublicSans-Italic.ttf" --output-file="PublicSans-Italic.woff" --flavor=woff --layout-features="ccmp,locl,mark,mkmk,kern" --desubroutinize --unicodes=U+0-10FFFF
+pyftsubset "PublicSans-Light.ttf" --output-file="PublicSans-Light.woff" --flavor=woff --layout-features="ccmp,locl,mark,mkmk,kern" --desubroutinize --unicodes=U+0-10FFFF
+pyftsubset "PublicSans-LightItalic.ttf" --output-file="PublicSans-LightItalic.woff" --flavor=woff --layout-features="ccmp,locl,mark,mkmk,kern" --desubroutinize --unicodes=U+0-10FFFF
+pyftsubset "PublicSans-Medium.ttf" --output-file="PublicSans-Medium.woff" --flavor=woff --layout-features="ccmp,locl,mark,mkmk,kern" --desubroutinize --unicodes=U+0-10FFFF
+pyftsubset "PublicSans-MediumItalic.ttf" --output-file="PublicSans-MediumItalic.woff" --flavor=woff --layout-features="ccmp,locl,mark,mkmk,kern" --desubroutinize --unicodes=U+0-10FFFF
+pyftsubset "PublicSans-Regular.ttf" --output-file="PublicSans-Regular.woff" --flavor=woff --layout-features="ccmp,locl,mark,mkmk,kern" --desubroutinize --unicodes=U+0-10FFFF
+pyftsubset "PublicSans-SemiBold.ttf" --output-file="PublicSans-SemiBold.woff" --flavor=woff --layout-features="ccmp,locl,mark,mkmk,kern" --desubroutinize --unicodes=U+0-10FFFF
+pyftsubset "PublicSans-SemiBoldItalic.ttf" --output-file="PublicSans-SemiBoldItalic.woff" --flavor=woff --layout-features="ccmp,locl,mark,mkmk,kern" --desubroutinize --unicodes=U+0-10FFFF
+pyftsubset "PublicSans-Thin.ttf" --output-file="PublicSans-Thin.woff" --flavor=woff --layout-features="ccmp,locl,mark,mkmk,kern" --desubroutinize --unicodes=U+0-10FFFF
+pyftsubset "PublicSans-ThinItalic.ttf" --output-file="PublicSans-ThinItalic.woff" --flavor=woff --layout-features="ccmp,locl,mark,mkmk,kern" --desubroutinize --unicodes=U+0-10FFFF
