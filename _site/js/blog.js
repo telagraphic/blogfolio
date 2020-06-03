d157dc7ac726952b469866987745bdb516cd5fbf
telagraphic <telagraphic@gmail.com>
2020-06-03 16:55:35 -0400

    🎞️ ANIMATION: portfolio animations

--- _site/js/blog.js
+++ _site/js/blog.js
@@ -1,3 +1,31 @@
+const projects = document.querySelectorAll('.work__project');
+
+projects.forEach(project => {
+  project.addEventListener('mouseenter', function(event) {
+    let picture = event.target.children[0].children[1].children[0];
+    let description = event.target.children[0].children[1].children[1];
+
+    gsap
+      .timeline()
+      .set(description, {opacity: 0})
+      .to(description, {opacity:1, duration: .5})
+      .to(description.children[0], {opacity:1, duration: .25}, '<');
+  })
+})
+
+projects.forEach(project => {
+  project.addEventListener('mouseleave', function(event) {
+    let picture = event.target.children[0].children[1].children[0];
+    let description = event.target.children[0].children[1].children[1];
+
+    gsap
+      .timeline()
+      .to(description, {opacity:0, duration: .25, ease: 'power2.out'})
+      .to(description.children[0], {opacity:0, duration: .25}, '>');
+  })
+})
+
+
 barba.init({
   transitions: [
     {
